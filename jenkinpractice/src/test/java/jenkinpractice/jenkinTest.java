49d045e5a7f7121a45ec6ab80982d6b028744e85
poonamacharekar <79695562+poonamacharekar@users.noreply.github.com>
2021-03-29 17:44:04 -1200

    change2

--- jenkinpractice/src/test/java/jenkinpractice/jenkinTest.java
+++ jenkinpractice/src/test/java/jenkinpractice/jenkinTest.java
@@ -1,12 +1,13 @@
 package jenkinpractice;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class jenkinTest {
 	
 @Test
 	public void jenkinpractice() {
-		System.out.print("poonam ");
+		Reporter.log("poonam ");
 	}
 }
 
